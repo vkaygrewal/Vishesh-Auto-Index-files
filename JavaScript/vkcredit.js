17a62f49d2fa2804b0b5a2729425e80b7b71e6c4
Vishesh Grewal <vkaygrewal@gmail.com>
2019-06-20 07:53:42 +0530

    Create vkcredit.js

---
+++ JavaScript/vkcredit.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function()
+{
+var aa=$("#mycredit").val();
+if (aa == null) {
+    window.location.href = "http://www.visheshgrewal.blogspot.com/";
+};
+ $("#mycredit").attr("href","
+http://www.visheshgrewal.blogspot.com/");
+
+});
+//]]>
+</script>
